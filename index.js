dc99f35a762a49006eadd3edbc1cf38d4b9cd4d3
Kevin Bravo <kbravo1024@gmail.com>
2023-03-14 23:51:11 -0700

    complete control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feetNum){
   // Write your code here!
+  if(feetNum <= 400){
+    return ("This one is on me!")
+  }
+  else if(feetNum > 400 && feetNum < 2000 ){
+    return("That will be twenty bucks.")
+  }
+  else if(feetNum >= 2000 && feetNum <= 2500){
+    return("I will gladly take your thirty bucks.")
+  }
+  else if(feetNum > 2500){
+    return("No can do.")
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(cityName){
   // Write your code here!
+  if(cityName == "NYC"){
+    return ("Ok, sounds good.")
+  }
+  else{
+    return ("No go.")
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tipMoney){
   // Write your code here!
+  if(tipMoney === "generous"){
+    return "Thank you so much."
+  }
+  else if(tipMoney === "not as generous"){
+    return "Thank you."
+  }
+  else{
+    return "Bye."
+  }
 }
